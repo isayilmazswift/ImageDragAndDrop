397443d28aeb26e8ecd14d218d97a6e48fc47e71
isa yilmaz <jesus.codes@icloud.com>
2021-03-26 10:45:41 +0300

    Initial Commit

---
+++ DragImage/DragImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragImageApp.swift
+//  DragImage
+//
+//  Created by İsa Yılmaz on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct DragImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
